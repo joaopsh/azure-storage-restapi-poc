f4c114d56683f0427d6b07a7845d8d9e72592a69
Joao Pedro Souza Homem <jpsouza@stone.com.br>
2018-08-21 19:41:24 -0300

    Set application's start.

--- AzureUploader/Program.cs
+++ AzureUploader/Program.cs
@@ -1,15 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
+using System.Threading;
+using AzureUploader.Core;
 
 namespace AzureUploader
 {
     class Program
     {
+        private static string _storageAccountName = "xxx";
+        private static string _storageAccountKey = "xxx";
+
         static void Main(string[] args)
         {
+            var storage = new AzureStorage(_storageAccountName, _storageAccountKey);
+
+            storage.UploadAsync(containerName: "test", 
+                blockName: "somefile.txt",
+                payload: Encoding.UTF8.GetBytes("Você já parou para imaginar que nesse exato momento existe alguém com a máquina travada por causa do Java?"),
+                cancellationToken: CancellationToken.None)
+                .GetAwaiter().GetResult();
         }
     }
 }
